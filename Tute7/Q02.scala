63883d404d26d1a338671a51001d29a49e037bd5
sandaresdf <sandaresdf3070@gmail.com>
2023-08-08 01:22:44 +0530

    Tutorial 4 submission

--- Tute7/Q02.scala
+++ Tute7/Q02.scala
@@ -8,6 +8,6 @@ object Q02 {
   def main(args: Array[String]): Unit = {
     val inputList = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     val outputList = calculateSquare(inputList)
-    println(outputList) // Output: List(2, 4, 6, 8, 10)
+    println(outputList)
   }
 }
